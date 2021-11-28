0c5c5fc740bd034efe0fd92d312f9531ddbdeaec
BrendanBarnard950 <68152705+BrendanBarnard950@users.noreply.github.com>
2021-11-28 17:08:34 +0200

    Only Random and Markdown Conversion left boiiis

--- Projects/Project1_WIKIPOODIA/encyclopedia/urls.py
+++ Projects/Project1_WIKIPOODIA/encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.article_view, name="article"),
+    path("search/", views.search_view, name="search"),
+    path("newpage", views.newpage_view, name="newpage")
 ]
--- Projects/Project1_WIKIPOODIA/encyclopedia/views.py
+++ Projects/Project1_WIKIPOODIA/encyclopedia/views.py
@@ -8,3 +8,45 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def article_view(request, title):
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/404.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/article.html", {
+            "contents": util.get_entry(title),
+            "title" : title
+        })
+
+
+def search_view(request):
+    search = request.GET.get('q')
+    entries = util.list_entries()
+    results = []
+    for entry in entries:
+        if search in entry:
+            results.append(entry)
+    if len(results) >1:
+        return render(request, "encyclopedia/index.html", {
+        "entries": results
+    })
+    elif len(results) ==1 and util.get_entry(search) == None:
+        return render(request, "encyclopedia/index.html", {
+        "entries": results
+    })
+    elif len(results) ==1:
+        return article_view(request, search)
+    else:
+        
+        return article_view(request, search)
+
+
+def newpage_view(request):
+    if request.method== "GET":
+        return render(request, "encyclopedia/newpage.html")
+    else:
+        util.save_entry(request.POST.get('title'), request.POST.get('content'))
+        return article_view(request, request.POST.get('title'))
+        
